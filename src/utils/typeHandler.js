79f9483796e85c4f587bd14e5895c06ec4a30076
onurkybsi <onurbpm@outlook.com>
2021-11-07 23:18:48 +0300

    getDefaultValue, hasDefaultValue methods was added to typeHandler module

--- src/utils/typeHandler.js
+++ src/utils/typeHandler.js
@@ -17,7 +17,29 @@ const getType = (value) => {
   return TypeNames.Object;
 };
 
+const getDefaultValue = (typeName) => {
+  if (getType(typeName) !== TypeNames.String)
+    throw new Error("typeName must be a string!");
+
+  if (typeName === TypeNames.String) return "";
+  if (typeName === TypeNames.Boolean) return false;
+  if (typeName === TypeNames.Function) return Function();
+  if (typeName === TypeNames.Number) return 0;
+  if (typeName === TypeNames.Object) return null;
+  if (typeName === TypeNames.Symbol) return Symbol();
+  if (typeName === TypeNames.Undefined) return undefined;
+  // TO-DO: new Class() default value will be added
+  throw new Error("typeName is unknown!");
+};
+
+const hasDefaultValue = (value) => {
+  let typeOfValue = getType(value);
+  return value === getDefaultValue(typeOfValue);
+};
+
 module.exports = {
   TypeNames,
   getType,
+  getDefaultValue,
+  hasDefaultValue,
 };
