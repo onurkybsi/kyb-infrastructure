da6e56993548a9160be395678b11070d530b333e
onurkybsi <onurbpm@outlook.com>
2021-11-26 00:43:59 +0100

    HttpClient.test continued

--- tests/utilities/HttpClient.test.ts
+++ tests/utilities/HttpClient.test.ts
@@ -1,4 +1,6 @@
-import { HttpClient, HttpMethods, InvalidParameterError } from "../../src/index";
+import { HttpClient, HttpMethods, HttpResponse, InvalidParameterError } from "../../src/index";
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 
 it('Throws InvalidParameterError if baseURL is empty', () => {
     expect(() => new HttpClient("")).toThrow(InvalidParameterError);
@@ -15,4 +17,15 @@ it('SendRequest throws InvalidParameterError if path is empty', async () => {
     } catch (error: any) {
         expect(error instanceof InvalidParameterError).toEqual(true);
     }
+})
+
+it('SendRequest returns isSuccessful false if an error occures', async () => {
+    // Arrange
+    let client: HttpClient = new HttpClient("https://google.com.tr");
+    let mockAdapter: MockAdapter = new MockAdapter(axios);
+    mockAdapter.onGet('https://google.com.tr').networkErrorOnce();
+    // Act
+    let res: HttpResponse = await client.SendRequest(HttpMethods.GET, "/");
+    // Assert
+    expect(res.isSuccessful).toEqual(false)
 })
